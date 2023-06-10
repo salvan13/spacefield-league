453bb82c8fd70a0c8d63fd4c7d409994524e7d47
Antonio Salvati <salvan13@gmail.com>
2023-06-10 16:16:20 +0200

    + eslint

--- shared/shared.js
+++ shared/shared.js
@@ -1,9 +1,9 @@
 export const V = [
-  { n: 'star-0', hit: 4, shot: 6, rec: 6, jump: 5 },
-  { n: 'cosmo-84', hit: 6, shot: 5, rec: 4, jump: 6 },
-  { n: 'super-p', hit: 2, shot: 10, rec: 2, jump: 2 },
-  { n: 'x-alpha', hit: 5, shot: 8, rec: 4, jump: 2 },
-  { n: 'exp-x', hit: 6, shot: 4, rec: 6, jump: 7 },
-  { n: 'exp-z', hit: 8, shot: 3, rec: 3, jump: 8 },
-  { n: 'focus-13', hit: 3, shot: 2, rec: 8, jump: 10 }
+  { n: "star-0", hit: 4, shot: 6, rec: 6, jump: 5 },
+  { n: "cosmo-84", hit: 6, shot: 5, rec: 4, jump: 6 },
+  { n: "super-p", hit: 2, shot: 10, rec: 2, jump: 2 },
+  { n: "x-alpha", hit: 5, shot: 8, rec: 4, jump: 2 },
+  { n: "exp-x", hit: 6, shot: 4, rec: 6, jump: 7 },
+  { n: "exp-z", hit: 8, shot: 3, rec: 3, jump: 8 },
+  { n: "focus-13", hit: 3, shot: 2, rec: 8, jump: 10 }
 ];
